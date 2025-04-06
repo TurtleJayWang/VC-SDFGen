3ec1f11685a8c08b934476bf576ac4e4257f89c7
TurtleJayWang <turtlejaywang@gmail.com>
2025-04-06 08:29:57 +0000

    Voxel Dataset added

--- data/dataset.py
+++ data/dataset.py
@@ -24,3 +24,24 @@ class ShapeNetSDF(Dataset):
     def load_npz_file(self, file_path):
         data = np.load(file_path)
         return torch.from_numpy(data["points"]), torch.from_numpy(data["sdfs"])
+
+class ShapeNetVoxel64(Dataset):
+    def __init__(self, path, used_categories="All"):
+        category_names = os.listdir(path)
+        self.voxels = []
+        for category_name in category_names:
+            model_file_names = os.listdir(os.path.join(path, category_name))
+            for model_file_name in model_file_names:
+                voxel_grid = self.load_npz_file(os.path.join(path, category_name, model_file_name))
+                self.voxels.append(voxel_grid)
+        self.n_models = len(self.voxels)
+        
+    def __len__(self):
+        return self.n_models
+    
+    def __getitem__(self, index):
+        return self.voxels[index], index
+    
+    def load_npz_file(self, file_path):
+        data = np.load(file_path)
+        return torch.from_numpy(data["voxel_data"]).float()
